0390467e5fae5ab157948eac9bb14f51b1474d71
sayem mahmud <sayem.mahmud97@gmail.com>
2023-03-24 02:43:49 +0600

    gits

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const request = require('request');
+
+const app = express();
+
+app.get('/download', async (req, res) => {
+    console.log('req', req.query);
+    const url = req.query?.url?.toString() ?? '';
+    const name = req.query?.name?.toString() ?? '';
+    res.setHeader('Content-Disposition', `attachment; filename=${name}`);
+    request
+        .get(url, { encoding: null, highWaterMark: 256 * 1024 })
+        .on('error', (err) => {
+            console.log(err);
+            res.status(500).send('Internal server error');
+        })
+        .pipe(res);
+});
+
+const PORT = process.env.PORT || 5000;
+app.get('/', (req, res) => {
+    res.send('Hello Doctors portal!')
+})
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
